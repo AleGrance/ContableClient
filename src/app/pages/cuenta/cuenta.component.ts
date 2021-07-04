836c1a6f1c15bb30d4fa7d9b12cb227cc23832e0
Ale Grance <alegrance1000@gmail.com>
2021-07-04 10:58:37 -0400

    registrar cuenta hijo y contable

--- src/app/pages/cuenta/cuenta.component.ts
+++ src/app/pages/cuenta/cuenta.component.ts
@@ -10,38 +10,84 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class CuentaComponent implements OnInit {
 
-  public cuentas: any;
-  public cuentaNueva = {
+  public cuentasMadre: any;
+  public cuentasHijo: any;
+  public cuentasContable: any;
+
+  public cuentaMadreNueva = {
     nombre_cuenta_madre: 'Nombre',
   };
-  public cuentaForm: any;
+  public cuentaHijoNueva = {
+    nombre_cuenta_hijo: 'Nombre',
+  };
+
+  public cuentaContableNueva = {
+    nombre_cuenta_contable: 'Nombre',
+  };
+
+  public cuentaMadreForm: any;
+  public cuentaHijoForm: any;
+  public cuentaContableForm: any;
 
   constructor(public api: ApiService) { }
 
   ngOnInit(): void {
 
-    this.cuentaForm = new FormGroup({
-      nombre: new FormControl(this.cuentaNueva.nombre_cuenta_madre, [
+    this.cuentaMadreForm = new FormGroup({
+      nombreCuentaMadre: new FormControl(this.cuentaMadreNueva.nombre_cuenta_madre, [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+    });
+
+    this.cuentaHijoForm = new FormGroup({
+      nombreCuentaHijo: new FormControl(this.cuentaHijoNueva.nombre_cuenta_hijo, [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+    });
+
+    this.cuentaContableForm = new FormGroup({
+      nombreCuentaContable: new FormControl(this.cuentaContableNueva.nombre_cuenta_contable, [
         Validators.required,
         Validators.minLength(4)
       ]),
     });
 
     // Trae datos del api
+    // Cuentas Madres
     this.api.get('cuenta_madre')
       .pipe(map(data => {
-        this.cuentas = data;
-        console.log(this.cuentas);
+        this.cuentasMadre = data;
+        console.log(this.cuentasMadre);
+      }))
+      .subscribe()
+
+    // Cuentas Hijos
+    this.api.get('cuenta_hijo')
+      .pipe(map(data => {
+        this.cuentasHijo = data;
+        console.log(this.cuentasHijo);
+      }))
+      .subscribe()
+
+    // Cuentas Contables
+    this.api.get('cuenta_contable')
+      .pipe(map(data => {
+        this.cuentasContable = data;
+        console.log(this.cuentasContable);
       }))
       .subscribe()
 
   }
 
-  get nombre() { return this.cuentaForm.get('nombre'); }
+  get nombreCuentaMadre() { return this.cuentaMadreForm.get('nombreCuentaMadre'); }
+  get nombreCuentaHijo() { return this.cuentaHijoForm.get('nombreCuentaHijo'); }
+  get nombreCuentaContable() { return this.cuentaContableForm.get('nombreCuentaContable'); }
 
-  submit() {
+  submitMadre() {
 
-    const nombre = ((<HTMLInputElement>document.getElementById("nombre")).value);
+    const nombre = ((<HTMLInputElement>document.getElementById("nombreCuentaMadre")).value);
 
     let newCuenta = {
       nombre_cuenta_madre: nombre
@@ -49,7 +95,37 @@ export class CuentaComponent implements OnInit {
 
     this.api.post('cuenta_madre', newCuenta)
       .subscribe(result => {
-        this.cuentas.push(result);
+        this.cuentasMadre.push(result);
+        console.log('result post: ', result);
+      });
+  }
+
+  submitHijo() {
+
+    const nombre = ((<HTMLInputElement>document.getElementById("nombreCuentaHijo")).value);
+
+    let newCuenta = {
+      nombre_cuenta_hijo: nombre
+    }
+
+    this.api.post('cuenta_hijo', newCuenta)
+      .subscribe(result => {
+        this.cuentasHijo.push(result);
+        console.log('result post: ', result);
+      });
+  }
+
+  submitContable() {
+
+    const nombre = ((<HTMLInputElement>document.getElementById("nombreCuentaContable")).value);
+
+    let newCuenta = {
+      nombre_cuenta_contable: nombre
+    }
+
+    this.api.post('cuenta_contable', newCuenta)
+      .subscribe(result => {
+        this.cuentasContable.push(result);
         console.log('result post: ', result);
       });
   }
