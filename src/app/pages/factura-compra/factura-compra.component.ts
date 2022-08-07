0a19b3b23e200f0e3438db3dab1b7cc72f3b5bf6
System Administrator <alegrance1000@gmail.com>
2022-08-07 19:46:31 -0400

    Form compras post a cabecera y detalle

--- src/app/pages/factura-compra/factura-compra.component.ts
+++ src/app/pages/factura-compra/factura-compra.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-factura-compra',
@@ -9,47 +11,32 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./factura-compra.component.css']
 })
 export class FacturaCompraComponent implements OnInit {
-  // El cliente encontrado mediante el id que le pasa cliente_component
+  // El contribuyente encontrado mediante el id que le pasa contribuyente_component, es para obtener los datos y mostrar en pantalla
   public contribuyenteEncontrado: any;
-  // El listado de contribuyentes
-  public contribuyentes: any;
+  // Se almacena el ID del contribuyente para enviar al grabar el registro
+  public contribuyenteId: any;
+
   // El listado de proveedores
   public proveedores: any;
   public proveedorId: any;
 
+  // La condicion en una variable
+  public condicion: any;
+
   // El subtotal calculado
   public subtotal = 0;
 
-  // Para este caso no se usa
-  /*public invoice = {
-    items: [{
-      name: 'item',
-      description: 'item description',
-      qty: 5,
-      price: 5.5
-    }]
-  }*/
-
-
-  constructor(public api: ApiService, private route: ActivatedRoute) { }
+  constructor(public api: ApiService, private route: ActivatedRoute, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     // Primero se obtiene el ID enviado a travez de la ruta
     const routeParams = this.route.snapshot.paramMap;
-    const contribuyenteIdFromRoute = Number(routeParams.get('id_contribuyente'));
-    // Luego se busca el registro en la base de dato a travez del API
-    this.api.get('contribuyente/' + contribuyenteIdFromRoute)
+    this.contribuyenteId = Number(routeParams.get('id_contribuyente')); //Este parametro se configura en app-routes
+    // Luego se busca el registro en la base de dato a travez del API para obtener los datos del contribuyente y mostrar en pantalla
+    this.api.get('contribuyente/' + this.contribuyenteId)
       .pipe(map(data => {
         this.contribuyenteEncontrado = data;
-        console.log("El contribuyente es: ", this.contribuyenteEncontrado);
-      }))
-      .subscribe()
-
-    // Trae datos del api
-    this.api.get('contribuyente')
-      .pipe(map(data => {
-        this.contribuyentes = data;
-        //console.log(this.contribuyentes);
+        //console.log("El contribuyente es: ", this.contribuyenteEncontrado);
       }))
       .subscribe()
 
@@ -57,63 +44,89 @@ export class FacturaCompraComponent implements OnInit {
     this.api.get('proveedor')
       .pipe(map(data => {
         this.proveedores = data;
-        console.log(this.proveedores);
+        //console.log(this.proveedores);
       }))
       .subscribe()
   }
 
-  // Al seleccionar el proveedor
+  // Al seleccionar el proveedor se guarda el ID en una variable para enviar a la base de datos
   onChangeProveedor(id: any) {
     this.proveedorId = parseInt(id);
-    console.log(this.proveedorId);
+    //console.log(this.proveedorId);
   }
 
-  // Calcula el subtotal
-  onChange() {
+  onChangeCondicion(value: any) {
+    this.condicion = value;
+    console.log(this.condicion);
+  }
+
+  // Calcula el subtotal al modificar el monto o el precio
+  onChangeCantidad() {
     const cantidad = parseInt((document.getElementById("cantidad") as HTMLInputElement).value);
     const precio = parseInt((document.getElementById("precio") as HTMLInputElement).value);
 
     this.subtotal = cantidad * precio;
-
-    console.log(cantidad, precio);
   }
 
   // Guardar los cambios
   grabar() {
+    // Datos para la cabecera
+    const nrofactura = (document.getElementById("nro") as HTMLInputElement).value;
+    const fecha = (document.getElementById("fecha") as HTMLInputElement).value;
+
+    //Datos para el detalle
+    const descripcion = (document.getElementById("descripcion") as HTMLInputElement).value;
+    const cantidad = (document.getElementById("cantidad") as HTMLInputElement).value;
+    const precio = (document.getElementById("precio") as HTMLInputElement).value;
+
+    // Objeto cabecera
     const cabeceraCompra = {
-      nro_factura_compra: "",
-      fecha_factura_compra: "",
-      condicion_venta_compra: "",
-      sub_total_compra: "",
-      iva_total_compra: "",
-      total_compra: "",
+      nro_factura_compra: nrofactura,
+      condicion_venta_compra: this.condicion,
+      fecha_factura_compra: fecha,
+      total_compra: this.subtotal,
+      // Campos relacionados
+      ContribuyenteIdContribuyente: this.contribuyenteId,
+      ProveedorIdProveedor: this.proveedorId
+    }
+
+    //Objeto detalle
+    const detalleCompra = {
+      descripcion_detalle_compra: descripcion,
+      cant_item_detalle_compra: cantidad,
+      subtotal_detalle_compra: this.subtotal,
+      precio_detalle_compra: precio,
+      nro_factura_compra: nrofactura
     }
 
     console.log(cabeceraCompra);
+    console.log(detalleCompra);
+
+
+    this.api.post('cabecera_compra', cabeceraCompra)
+      .subscribe(result => {
+        // Se actualiza la vista html si el result retorna un objeto, significa que inserto en la bd. De lo contrario muestra el mensaje de error que retorna el server
+        if (typeof result === 'object') {
+          this.toastr.success('Compra registrada');
+          // Solo si cumple la condicion se registra el detalle
+          this.api.post('detalle_compra', detalleCompra)
+            .subscribe(result => {
+            }, error => {
+              console.log('Si hay error en el post: ', error);
+            });
+
+          // Llama a la funcion onInit que resetea el formulario
+          this.ngOnInit();
+        } else {
+          console.log('result post: ', result);
+          this.toastr.warning(result);
+        }
+      }, error => {
+        console.log('Si hay error en el post: ', error);
+      });
+
+
 
   }
-
-  cargarClienteSeleccionado(value: any) {
-    console.log("Datos recibidos de cliente component: ", value);
-  }
-
-  // Agregar item // Para este caso no se va usar
-  /*add() {
-    this.invoice.items.push({
-      name: '',
-      description: '',
-      qty: 1,
-      price: 0
-    })
-
-    console.log(this.invoice.items);
-  }*/
-
-  // Eliminar item
-  /*remove(index: any) {
-    console.log(index);
-    this.invoice.items.splice(index, 1);
-    console.log(this.invoice.items);
-  }*/
 
 }
