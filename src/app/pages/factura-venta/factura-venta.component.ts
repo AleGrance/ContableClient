0819977d8cd873662f256cb78f3cf8040fc04bfe
System Administrator <alegrance1000@gmail.com>
2022-08-08 13:23:28 -0400

    Factura venta variable

--- src/app/pages/factura-venta/factura-venta.component.ts
+++ src/app/pages/factura-venta/factura-venta.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-factura-venta',
@@ -6,6 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./factura-venta.component.css']
 })
 export class FacturaVentaComponent implements OnInit {
+  // El contribuyente encontrado mediante el id que le pasa contribuyente_component, es para obtener los datos y mostrar en pantalla
+  public contribuyenteEncontrado: any;
+  // Se almacena el ID del contribuyente para enviar al grabar el registro
+  public contribuyenteId: any;
+
+  // El listado de clientes
+  public clientes: any;
+  public clienteId: any;
+
+  // La condicion en una variable
+  public condicion: any;
+
+  // El subtotal calculado
+  public subtotal = 0;
 
   constructor() { }
 
